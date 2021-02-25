830b5a9622d295096c54f8050cad4ebd9fbab7df
proatgram <34280824+proatgram@users.noreply.github.com>
2021-02-24 21:37:15 -0600

    Revised a temparary solution for the warning when trying to compile Game.cpp and Item.h

--- source/Inventory.h
+++ source/Inventory.h
@@ -13,23 +13,25 @@ public:
   }
 
   int getSlotNum(const std::string& name, const int itemID) const {
-    
+    return 0;
    }
   const std::string& getName(const int itemSlot, const int itemID) const {
-  
+    const std::string& temp = "0";
+    return temp;
   }
   const std::string& getItemClass(const std::string& name, const int itemSlot, const int temID) const {
-    
-    }
+    const std::string& temp = "0";
+    return temp;
+  }
   int getDurability(const std::string& name, const int itemID) const {
-
+    return 0;
   }
   void setItem(const std::string& itemName, const int itemID, const int itemDurability, const int itemDamage, const std::string& itemClass) {
-
+    
   }
 
   void removeItem(const std::string itemName, const int itemSlot, const int itemID){
-
+    
   }
 protected:
 
