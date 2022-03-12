84fc79c109d2a16fabeb869c61bd7a8a723f2bbf
Timothy Hutchins <tgh6407@gmail.com>
2022-03-11 18:46:25 -0600

    Started implementing World Generation class.

--- src/World.cpp
+++ src/World.cpp
@@ -1,8 +1,10 @@
 #include "World.h"
 
-World::World(int size_x, int size_y) :
+World::World(int size_x, int size_y, int seed) :
 	m_size_x(size_x),
-	m_size_y(size_y)
+	m_size_y(size_y),
+	m_generate(seed)
+
 {
 
 }
